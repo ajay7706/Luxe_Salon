cd12d80358b33b38b15f1c358af03ce68aee9e92
Akshay Kumar <ajayk043086@gmail.com>
2026-02-28 02:32:38 +0530

    backend-push

--- Backend/server.js
+++ Backend/server.js
@@ -125,7 +125,7 @@ app.use((err, req, res, next) => {
    SERVER START
 ============================== */
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 18012;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
