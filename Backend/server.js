5c6126f591196d79c21e79b06823e5b946ba639c
Akshay Kumar <ajayk043086@gmail.com>
2026-03-01 15:44:26 +0530

    relax-cors-headers-auto

--- Backend/server.js
+++ Backend/server.js
@@ -15,8 +15,6 @@ app.use(
   cors({
     origin: true,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.json());
